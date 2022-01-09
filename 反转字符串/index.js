4874acdd29406c96392dd152fc0dd92e0da847a8
wuxiaolivv <bestwuxiali@gmail.com>
2022-01-09 16:08:00 +0800

    feat: 提交代码

--- 反转字符串/index.js
+++ 反转字符串/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-08 23:01:30
- * @LastEditTime: 2022-01-08 23:25:05
+ * @LastEditTime: 2022-01-09 15:05:44
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /Algorithms-Data-Structures/反转字符串/index.js
@@ -29,3 +29,7 @@ const reverse2 = (arr) => {
 };
 
 reverse2("123");
+
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
